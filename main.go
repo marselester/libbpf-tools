e06fd7f06797b0f3d03f8225f729a0be9ee24585
marselester <marselester@gmail.com>
2021-05-14 11:51:51 -0400

    Read from perf PERF_EVENT_ARRAY map

--- main.go
+++ main.go
@@ -3,10 +3,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"log"
-	"time"
+	"os"
 
 	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
 	"golang.org/x/sys/unix"
 )
 
@@ -37,19 +40,56 @@ func main() {
 	defer kp.Close()
 
 	log.Println("Waiting for events...")
-	var key uint64
+
+	// Open a perf event reader from userspace on the PERF_EVENT_ARRAY map
+	// defined in the BPF C program.
+	rd, err := perf.NewReader(objs.TCPConnLatMaps.Events, os.Getpagesize())
+	if err != nil {
+		log.Fatalf("creating perf event reader: %s", err)
+	}
+	defer rd.Close()
+
 	for {
-		var value piddata
-		if err := objs.TCPConnLatMaps.Start.Lookup(key, &value); err != nil {
-			log.Fatalf("reading map: %v", err)
+		var v event
+
+		record, err := rd.Read()
+		if err != nil {
+			if perf.IsClosed(err) {
+				return
+			}
+			log.Printf("reading from perf event reader: %v", err)
 		}
-		log.Printf("tcp_v6_connect called: %v\n", value)
-		time.Sleep(time.Second)
+
+		if record.LostSamples != 0 {
+			log.Printf("rind event perf buffer is full, dropped %d samples", record.LostSamples)
+			continue
+		}
+
+		err = binary.Read(
+			bytes.NewBuffer(record.RawSample),
+			binary.LittleEndian,
+			&v,
+		)
+		if err != nil {
+			log.Printf("failed to parse perf event: %v", err)
+			continue
+		}
+
+		log.Println(v)
 	}
 }
 
-type piddata struct {
-	comm [16]byte
-	ts   uint64
-	tgid uint32
+// event represents a perf event sent to userspace from the BPF program running in the kernel.
+// Note, that it must match the C event struct, and both C and Go structs must be aligned the same way.
+type event struct {
+	saddrV4 uint32
+	saddrV6 [16]byte
+	daddrV4 uint32
+	daddrV6 [16]byte
+	comm    [16]byte
+	deltaUs uint64
+	tsUs    uint64
+	tgid    uint32
+	af      int
+	dport   uint16
 }
